bf281a4e9588eaac63cc035d7146af90f584d183
LVNAnh <levunhatanh1997@gmail.com>
2025-02-28 13:26:04 +0700

    hehe

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello Wordd");
+  res.send("Hello World");
 });
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });

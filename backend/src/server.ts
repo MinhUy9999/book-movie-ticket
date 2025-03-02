cc0bed51ab0f37ef167b77914993193c5c111cc8
MinhUy9999 <154686164+MinhUy9999@users.noreply.github.com>
2025-03-02 09:35:14 +0700

    devUypush

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // commit file 
 // heheeh
+// huuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
 app.use(express.json()); // Middleware để parse JSON
 app.use(cookieParser()); // Middleware để parse cookie
 app.use("/api/users", userRoutes);

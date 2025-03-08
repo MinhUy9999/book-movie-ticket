a1ce09fb21756d09e40794e0f4eda97eab83aafb
MinhUy9999 <154686164+MinhUy9999@users.noreply.github.com>
2025-03-08 13:39:36 +0700

    them icons

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,10 +7,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); 
-app.use(cookieParser()); 
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", router);
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
